2822e169dfd4992b56124a8c92ab7d6ea9c65424
Ersin Esen <ersinesen@gmail.com>
2024-03-24 13:48:26 +0000

    new cell types added: chart, youtube

--- README.md
+++ README.md
@@ -2,11 +2,15 @@
 
 Javascript Table with different components using Tabler UI components.
 
+![A sample](jtabula.png)
+
 ## Usage
 
 In header
 ```
 <script src="https://cdn.jsdelivr.net/npm/@tabler/core@latest/dist/js/tabler.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/core@latest/dist/css/tabler.min.css">
 
 <style>
@@ -21,7 +25,7 @@ In header
     font-feature-settings: "cv03", "cv04", "cv11";
   }
 </style>
-<link href="style.css" rel="stylesheet" type="text/css" />
+<link href="jtabula.css" rel="stylesheet" type="text/css" />
 ```
 
 In body
@@ -30,22 +34,48 @@ In body
 
 <script src="jtabula.js"></script>
 <script>
-  // Our table
-  const containerId = "jtabula-container";
-  const table = new JTabula(containerId, 3);
-  table.createTable();
-  const cellDataList = [
-    {type: "text", data: "Text Cell 1", disabled: false},
-    {type: "input", data: "Input Cell", disabled: true},
-    {type: "select", data: ["Option 1", "Option 2", "Option 3"], disabled: false},
-    {type: "img", data: "https://via.placeholder.com/120", disabled: false},
-    {type: "select", data: ["Option 11", "Option 21", "Option 31"], disabled: false},
-  ];
+  // Create table
+    document.addEventListener("DOMContentLoaded", function () {
+      const containerId = "jtabula-container";
+      const table = new JTabula(containerId, 3);
+      table.createTable();
+      const cellDataList = [
+        {type: "chart"},
+        {type: "youtube", data: "https://www.youtube.com/embed/R2DU85qLfJQ"},
+        {type: "text", data: "Text Cell 1"},
+        {type: "text", data: "Cell 2"},
+        {type: "text", data: "Text Cell 3"},
+        {type: "input", data: "Input Cell", disabled: true},
+        {type: "text", data: "Cell 4"},
+        {type: "img", data: "https://via.placeholder.com/120"},
+        {type: "img", data: "https://via.placeholder.com/150"},
+        {type: "img", data: "https://via.placeholder.com/180"},
+        {type: "select", data: ["Option 11", "Option 21", "Option 31"], disabled: true, hAlign: "right"},
+        {type: "img", data: "https://via.placeholder.com/190"},
+        {type: "select", data: ["Option 1", "Option 2", "Option 3"], hAlign: "right"},
+        {type: "input", data: "Input"},
+      ];
 
-  cellDataList.forEach(cell => {
-    table.appendCell(cell);
-  });
-  console.log(table.getCellValue(0, 2));
-  console.log(table.getCellValue(1, 0)); 
-  </script>
+      table.setCellDimensions("240px", "180px");
+      cellDataList.forEach(cell => {
+        table.appendCell(cell);
+      });
+      console.log(table.getCellValue(1, 0));
+    });
+</script>
   ```
+
+  ## License
+
+  ```
+  * MIT License
+   *
+   * Copyright 2024 Ersin Esen
+   *
+   * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+   *
+   * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+   *
+   * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+```
--- jtabula.js
+++ jtabula.js
@@ -1,27 +1,72 @@
+/**
+ * Represents a dynamic table generator.
+ * @class
+ * @author Ersin Esen
+ * @see {@link https://github.com/ersinesen}
+ * @since March 24, 2024
+ * @license MIT
+ *
+ * MIT License
+ *
+ * Copyright 2024 Ersin Esen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 class JTabula {
-  constructor(containerId, columnsPerRow, tableID = "customTable") {
+  /**
+   * Create a JTabula instance.
+   * @constructor
+   * @param {string} containerId - The ID of the container where the table will be appended.
+   * @param {number} columnsPerRow - The number of columns per row in the table.
+   * @param {string} [tableID="jtTable"] - The ID to be assigned to the generated table.
+   */
+  constructor(containerId, columnsPerRow, tableID = "jtTable") {
     this.containerId = containerId;
     this.columnsPerRow = columnsPerRow;
     this.tableID = tableID;
-    this.columnData = [];
+    this.cellWidth = null;
+    this.cellHeight = null;
   }
 
-  setColumnData(columnData) {
-    this.columnData = columnData;
-  }
-
+  /**
+   * Creates the table structure within the specified container.
+   */
   createTable() {
     const container = document.getElementById(this.containerId);
     const table = document.createElement("table");
-    table.classList.add("custom-table", "table", "table-borderless");
     table.id = this.tableID;
-
     const tbody = document.createElement("tbody");
     table.appendChild(tbody);
-
     container.appendChild(table);
   }
 
+  /**
+   * Sets custom dimensions for the cells.
+   * @param {string} width - The width of the cells.
+   * @param {string} height - The height of the cells.
+   */
+  setCellDimensions(width, height) {
+    this.cellWidth = width;
+    this.cellHeight = height;
+    // Set custom properties for cell width and height
+    document.documentElement.style.setProperty('--cell-width', width);
+    document.documentElement.style.setProperty('--cell-height', height);
+  }
+
+  /**
+   * Appends a cell to the table.
+   * @param {Object} cell - The cell object to be appended.
+   * @param {string} cell.type - The type of the cell (text, input, select, img, chart, youtube).
+   * @param {string} [cell.data] - The data to be displayed in the cell (for text, input, img, and youtube types).
+   * @param {boolean} [cell.disabled] - Whether the cell is disabled (for input and select types).
+   * @param {string} [cell.hAlign] - The horizontal alignment of the cell content.
+   * @param {string} [cell.vAlign] - The vertical alignment of the cell content.
+   */
   appendCell(cell) {
     const table = document.getElementById(this.tableID);
     const rows = table.rows;
@@ -33,18 +78,31 @@ class JTabula {
     const cellIndex = lastRow.cells.length;
     const newCell = lastRow.insertCell(cellIndex);
     newCell.id = `cell-${rows.length - 1}-${cellIndex}`;
+    newCell.style.textAlign = cell.hAlign ? cell.hAlign : "center";
+    newCell.style.verticalAlign = cell.vAlign ? cell.vAlign : "middle";
+    newCell.classList.add("jt-cell");
+
+    // fill in data
     this.populateCell(newCell, cell);
   }
 
+  /**
+   * Populates a cell based on its type.
+   * @param {HTMLElement} cell - The cell element to be populated.
+   * @param {Object} cellData - The data object for the cell.
+   * @param {string} cellData.type - The type of the cell (text, input, select, img, chart, youtube).
+   * @param {*} [cellData.data] - The data to be displayed in the cell (for text, input, img, and youtube types).
+   * @param {boolean} [cellData.disabled] - Whether the cell is disabled (for input and select types).
+   */
   populateCell(cell, cellData) {
     cell.type = cellData.type;
-    cell.classList.add("jt-cell");
     switch (cellData.type) {
       case "text":
         cell.textContent = cellData.data;
         break;
       case "input":
         const input = document.createElement("input");
+        input.classList.add("jt-input");
         input.type = "text";
         input.value = cellData.data;
         input.disabled = cellData.disabled || false;
@@ -72,11 +130,37 @@ class JTabula {
         img.alt = "Image";
         cell.appendChild(img);
         break;
+      case "chart":
+        const chartDiv = document.createElement("div");
+        chartDiv.id = `chart-${cell.id}`;
+        cell.appendChild(chartDiv);
+        this.createChart(chartDiv);
+        break;
+      case "youtube":
+        // Create the iframe element
+        const iframe = document.createElement("iframe");
+        iframe.classList.add("jt-iframe");
+        iframe.src = cellData.data;
+        // Set the attributes for the iframe
+        //iframe.width = "1619";
+        //iframe.height = "911";
+        iframe.src = "https://www.youtube.com/embed/R2DU85qLfJQ";
+        iframe.title = "Our Planet | Fresh Water | FULL EPISODE | Netflix";
+        iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share";
+        iframe.allowFullscreen = false;
+        cell.appendChild(iframe);
+        break;
       default:
         cell.textContent = "Invalid cell type";
     }
   }
 
+  /**
+   * Retrieves the value of a cell based on its row and column indices.
+   * @param {number} row - The row index of the cell.
+   * @param {number} col - The column index of the cell.
+   * @returns {*} The value of the cell.
+   */
   getCellValue(row, col) {
     const table = document.getElementById(this.tableID);
     if (row >= 0 && row < table.rows.length && col >= 0 && col < this.columnsPerRow) {
@@ -98,4 +182,25 @@ class JTabula {
     return null;
   }
 
+  /**
+   * Creates a chart within a specified div element.
+   * @param {HTMLElement} div - The div element to contain the chart.
+   */
+  createChart(div) {
+    // Create a new chart
+    if (window.ApexCharts) {
+      let myChart = new ApexCharts(div, {
+        chart: {
+          type: 'bar',
+        },
+        series: [{
+          data: [18, 28, 47, 57, 77]
+        }],
+        yaxis: {
+          opposite: true,
+        }
+      })
+      myChart.render();
+    }
+  }
 }
